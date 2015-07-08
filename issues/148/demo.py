79631954cbea37131c34e349cd9b0283f2a63183
0u812 <0u812@users.noreply.github.com>
2015-07-08 12:11:27 -0700

    fix model path

--- issues/148/demo.py
+++ issues/148/demo.py
@@ -2,6 +2,6 @@
 
 import roadrunner as rr
 
-r = rr.RoadRunner('/home/poltergeist/devel/models/kaylene/issue-148-2.xml')
+r = rr.RoadRunner('../../models/issue-148-2.xml')
 print('getReactionIds = {}'.format(r.model.getReactionIds()))
 print('getReactionRates = {}'.format(r.model.getReactionRates()))
