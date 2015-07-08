8e1152c18b13efc54219e8a207a1c07e002d6afc
0u812 <0u812@users.noreply.github.com>
2015-07-08 12:12:47 -0700

    call functions on rr (should fail)

--- issues/148/demo.py
+++ issues/148/demo.py
@@ -3,5 +3,10 @@
 import roadrunner as rr
 
 r = rr.RoadRunner('../../models/issue-148-2.xml')
+# call on model
 print('getReactionIds = {}'.format(r.model.getReactionIds()))
 print('getReactionRates = {}'.format(r.model.getReactionRates()))
+
+# call on rr
+print('getReactionIds = {}'.format(r.getReactionIds()))
+print('getReactionRates = {}'.format(r.getReactionRates()))
